843a9d580a3520d70cd7aba4de1e6d47a9589aa8
Dhairya-7 <77637920+Dhairya-7@users.noreply.github.com>
2021-08-15 20:42:00 +0530

    Created Method

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
  
+class Add{
+    int total=0;
+        Add(String str){
+            if(str=="" || str==" "){
+                System.out.println("0");
+            }
+            else{
+
+            String[] arr=str.split(",");
+
+            for(int i=0;i<arr.length;i++){
+                // System.out.println(arr[i]+" abcd");
+                int a=Integer.parseInt(arr[i]);
+                total+=a;
+                // System.out.println(a);
+            }
+                System.out.println(total);
+            }
+        }
+}
+
 public class StringCalculator
 {
     public static void main(String[] args)
     {
         String str = "1,2";
-        System.out.println(str);
-        int add=0;
-
-        String[] arr=str.split(",");
-
-        for(int i=0;i<arr.length;i++){
-            // System.out.println(arr[i]+" abcd");
-            int a=Integer.parseInt(arr[i]);
-            add+=a;
-            // System.out.println(a);
-        }
-            System.out.println(add);
-        
+        // String str="";
+        // System.out.println(str);
+        Add a=new Add(str); 
     }
 }
